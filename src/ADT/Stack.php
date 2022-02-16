2ba011427d28bfd6120e8b51aa9bc6ab9697a70a
Mark Bradley <braddle@gmail.com>
2022-02-16 09:52:46 +0000

    Our implmentation of a Stack. Go TEAM

--- src/ADT/Stack.php
+++ src/ADT/Stack.php
@@ -1,8 +1,28 @@
 <?php
-
 namespace Braddle\ADT;
 
 class Stack
 {
+    private $size = 0;
+    private $value = [];
 
+    public function isEmpty()
+    {
+        return $this->size == 0;
+    }
+
+    public function push(string $input)
+    {
+        $this->value[$this->size++] = $input;
+    }
+
+    public function size() : int
+    {
+        return $this->size;
+    }
+
+    public function pop()
+    {
+        return $this->value[--$this->size];
+    }
 }
--- test/ADT/StackTest.php
+++ test/ADT/StackTest.php
@@ -6,4 +6,56 @@ use PHPUnit\Framework\TestCase;
 class StackTest extends TestCase
 {
 
+    private $empty, $one, $three;
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->empty = new Stack();
+
+        $this->one = new Stack();
+        $this->one->push("pop");
+
+        $this->three = new Stack();
+        $this->three->push("snap");
+        $this->three->push("crackle");
+        $this->three->push("pop");
+    }
+
+    public function testIsEmpty()
+    {
+        $this->assertTrue($this->empty->isEmpty());
+    }
+
+    public function testNotEmpty()
+    {
+        $this->assertFalse($this->one->isEmpty());
+    }
+
+    public function testSize()
+    {
+        $this->assertEquals(0, $this->empty->size());
+        $this->assertEquals(1, $this->one->size());
+        $this->assertEquals(3, $this->three->size());
+    }
+
+    public function testSinglePop()
+    {
+        $this->assertEquals("pop", $this->one->pop());
+        $this->assertTrue($this->one->isEmpty());
+        $this->assertEquals(0, $this->one->size());
+    }
+
+    public function testMultiPop()
+    {
+        $this->assertEquals("pop", $this->three->pop());
+        $this->assertEquals(2, $this->three->size());
+
+        $this->assertEquals("crackle", $this->three->pop());
+        $this->assertEquals(1, $this->three->size());
+
+        $this->assertEquals("snap", $this->three->pop());
+        $this->assertEquals(0, $this->three->size());
+        $this->assertTrue($this->three->isEmpty());
+    }
 }
